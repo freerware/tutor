c1d1171df683260c1e96cc58177478a123988357
fr33r <github@fr33r.com>
2025-03-22 23:56:34 -0700

    Fix MySQL grants and remove ACCOUNT table init SQL.

--- docker/mysql/startup_scripts/2_mysql_grant_user_privileges.sql
+++ docker/mysql/startup_scripts/2_mysql_grant_user_privileges.sql
@@ -1,6 +1,6 @@
 /*
 	Grant appropriate privileges for web_app user.
 */
--- REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'app_user'@'%'; -- since web_app is created using $MYSQL_USER and $MYSQL_PASSWORD, it has GRANT ALL applied which isn't good.
--- GRANT SELECT, INSERT, UPDATE, DELETE on safest-watch.* TO 'app_user'@'%'; -- adding back the privileges that are more restrictive.
--- FLUSH PRIVILEGES;
+REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'web_app'@'%'; -- since web_app is created using $MYSQL_USER and $MYSQL_PASSWORD, it has GRANT ALL applied which isn't good.
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES on tutor.* TO 'web_app'@'%'; -- adding back the privileges that are more restrictive.
+FLUSH PRIVILEGES;
